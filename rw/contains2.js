3361428925ab73ece41f83dc67829b686fca547a
kris <hezhi66327@163.com>
2023-08-09 15:55:41 +0800

    通用

--- rw/contains2.js
+++ rw/contains2.js
@@ -1,23 +1,28 @@
+/*
 
-var responseData = {
-    "message": "",
-    "data": {
-      "id": 1337564,
-      "apple_mac_subscription": null,
-      "referee_pro_days": 9999,
-      "register_via": "ios",
-      "apple_subscription": null,
-      "created_at": "2023-07-22 15:20:50",
-      "wechat_nickname": null,
-      "email_verified_at": "2023-07-22 15:20:50",
-      "pro_expired_at": "2099-09-09 23:59:59",
-      "api_token": "531d142cfccac81f6920bf0c7ba1ef72",
-      "language": "zh",
-      "slug": "MTMzNzU2NA",
-      "google_play_subscription": null,
-      "referer_id": null
-    },
-    "code": 0
-  };
+[rewrite_local]
 
-  $done({ body: JSON.stringify(responseData) });
+通用订阅解锁
+^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body sub.js
+
+[mitm]
+hostname = buy.itunes.apple.com
+
+*/
+let body= $response.body;
+var obj = JSON.parse(body);
+if (body.indexOf("expires") !=-1) {
+  obj["receipt"]["in_app"][0]["expires_date"] = "2099-09-09 20:12:02 Etc/GMT";
+  obj["receipt"]["in_app"][0]["expires_date_pst"] = "2099-09-09 12:12:02 America/Los_Angeles";
+  obj["receipt"]["in_app"][0]["expires_date_ms"] = "4092647115000";
+  obj["receipt"]["in_app"][0]["is_trial_period"] = "false";
+  obj["receipt"]["in_app"][0]["is_in_intro_offer_period"] = "false";
+  obj["receipt"]["in_app"][0]["in_app_ownership_type"] = "PURCHASED";
+  obj["latest_receipt_info"][0]["expires_date"] = "2099-09-09 20:12:02 Etc/GMT";
+  obj["latest_receipt_info"][0]["expires_date_pst"] = "2099-09-09 12:12:02 America/Los_Angeles";
+  obj["latest_receipt_info"][0]["expires_date_ms"] = "4092647115000";
+  obj["latest_receipt_info"][0]["is_trial_period"] = "false";
+  obj["latest_receipt_info"][0]["is_in_intro_offer_period"] = "false";
+  obj["latest_receipt_info"][0]["in_app_ownership_type"] = "PURCHASED";
+  }
+$done({body: JSON.stringify(obj)});
